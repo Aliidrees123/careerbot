1c923e04003191bb415e635ac7d5dc46338e2287
Ali Idrees <aliidrees394@gmail.com>
2026-02-19 22:01:57 +0000

    Updated readme and added app.py

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ---
 title: CareerBot
-app_file: src/careerbot/main.py
+app_file: app.py
 sdk: gradio
 sdk_version: 6.5.1
+python_version: "3.12"
 ---
---
+++ app.py
@@ -0,0 +1,5 @@
+# app.py
+from careerbot.main import orchestrator  # whatever your Gradio object is called
+
+if __name__ == "__main__":
+    orchestrator.launch()
